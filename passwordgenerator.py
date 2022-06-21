93aa9845f8a51c703ada89d6cd0b5f250912674a
ThatGuyCalledJesse <72275409+ThatGuyCalledJesse@users.noreply.github.com>
2022-06-21 21:04:02 +0200

    Updated the GUI version to a more optimal version

--- passwordgenerator.py
+++ passwordgenerator.py
@@ -1,9 +1,7 @@
-while True:
-    try:
-        import random
-        import os
+import random
+import os
 
-        possibleCharacters = ['a', 'b', 'c', 'd', 'e',
+possibleCharacters = ['a', 'b', 'c', 'd', 'e',
                               'f', 'g', 'h', 'i', 'j',
                               'k', 'l', 'm', 'n', 'o',
                               'p', 'q', 'r', 's', 't',
@@ -12,8 +10,10 @@ while True:
                               '5', '6', '7', '8', '9',
                               '0', '!', '@', '#', '$',
                               '%', '^', '&', '*', '?']
-        password = ""
+password = ""
 
+while True:
+    try:
         passLength = input("How long should the password be?")
         passLength = int(passLength)
         generatedPassLength = 0
