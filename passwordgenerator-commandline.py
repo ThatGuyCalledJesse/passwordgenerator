ace5c5ccd0d63786f01d1f97c53b19191d515f88
ThatGuyCalledJesse <72275409+ThatGuyCalledJesse@users.noreply.github.com>
2022-06-28 13:28:25 +0200

    Add files via upload

--- passwordgenerator-commandline.py
+++ passwordgenerator-commandline.py
@@ -2,25 +2,32 @@ import random
 import os
 import argparse
 
+parser = argparse.ArgumentParser(description="Determines the length of the generated password")
+parser.add_argument('Length', metavar='Length', type=int, help="Enter the desired length")
+args = parser.parse_args()
+
 possibleCharacters = ['a', 'b', 'c', 'd', 'e',
                       'f', 'g', 'h', 'i', 'j',
                       'k', 'l', 'm', 'n', 'o',
                       'p', 'q', 'r', 's', 't',
                       'u', 'v', 'w', 'x', 'y',
-                      'z', '1', '2', '3', '4',
-                      '5', '6', '7', '8', '9',
-                      '0', '!', '@', '#', '$',
-                      '%', '^', '&', '*', '?']
+                      'z']
+numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+specials = ['!', '@', '#', '$', '%', '^', '&', '*', '?']
 password = ""
+numbersEnabled = input("Do you want to enable numbers? [YES] [NO]\n>")
+specialsEnabled = input("Do you want to enable specials? [YES] [NO]\n>")
 
-parser = argparse.ArgumentParser(description="Determines the length of the generated password")
-parser.add_argument('Length', metavar='Length', type=int, help="Enter the desired length")
-args = parser.parse_args()
+if numbersEnabled.lower() == "yes":
+    for number in numbers:
+        possibleCharacters.append(number)
+if specialsEnabled.lower() == "yes":
+    for special in specials:
+        possibleCharacters.append(special)
 
 try:
     passLength = args.Length
     generatedPassLength = 0
-
     while generatedPassLength < passLength:
         character = random.choice(possibleCharacters)
         password = password + character
--- passwordgenerator.py
+++ passwordgenerator.py
@@ -6,15 +6,22 @@ possibleCharacters = ['a', 'b', 'c', 'd', 'e',
                       'k', 'l', 'm', 'n', 'o',
                       'p', 'q', 'r', 's', 't',
                       'u', 'v', 'w', 'x', 'y',
-                      'z', '1', '2', '3', '4',
-                      '5', '6', '7', '8', '9',
-                      '0', '!', '@', '#', '$',
-                      '%', '^', '&', '*', '?']
+                      'z']
+numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+specials = ['!', '@', '#', '$', '%', '^', '&', '*', '?']
 password = ""
 
 while True:
     try:
         passLength = input("How long should the password be?")
+        numbersEnabled = input("Do you want to enable numbers? [YES] [NO]\n>")
+        specialsEnabled = input("Do you want to enable special characters? [YES] [NO]\n>")
+        if numbersEnabled.lower() == "yes":
+            for number in numbers:
+                possibleCharacters.append(number)
+        if specialsEnabled.lower() == "yes":
+            for special in specials:
+                possibleCharacters.append(special)
         passLength = int(passLength)
         generatedPassLength = 0
 
